4a9ef9ad29a4660148aaf15b1ec391a6775ebb75
吴学谦 <wuxueqian@ezbuy.com>
2017-12-21 14:05:12 +0800

    koa for ssr server

--- ssr/index.ts
+++ ssr/index.ts
@@ -1,30 +1,36 @@
 import * as path from "path";
-import * as express from "express";
-import * as bodyParser from "body-parser";
-// import * as cookieParser from "cookie-parser";
-import * as compression from "compression";
-import * as responseTimer from "response-time";
+import * as koa from "koa";
+import * as koaStatic from "koa-static";
+import * as bodyParser from "koa-bodyparser";
 import ssrRouter from "./render";
 import { SSR_SERVER_HOST, SSR_SERVER_PORT } from "../env";
 
-let app = express();
-app.use(compression());
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
-// app.use(cookieParser());
-app.use(responseTimer());
+let app = koa();
+app.use(bodyParser());
+
+// x-response-time
+app.use(function*(next) {
+    // (1) 进入路由
+    var start = new Date().getTime();
+    yield next;
+    // (5) 再次进入 x-response-time 中间件，记录2次通过此中间件「穿越」的时间
+    var ms = new Date().getTime() - start;
+    this.set("X-Response-Time", ms + "ms");
+    // (6) 返回 this.body
+});
 
 app.use(
-    express.static(path.resolve(__dirname, "../dist"), {
+    koaStatic(path.resolve(__dirname, "../dist"), {
+        gzip: true,
         index: "",
-        maxAge: 31536000000, // milliseconds
-        etag: false,
-        lastModified: false
+        maxage: 31536000000 // milliseconds
     })
 );
+
 app.get(/^[^.]+$/, ssrRouter);
 
 app.disable("x-powered-by");
+
 app.listen(SSR_SERVER_PORT, SSR_SERVER_HOST, err => {
     if (err) {
         return console.error(err);
